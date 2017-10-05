be52cf7d10f577daa6d574c09bdc3550e2d915b6
Marty Chao <marty@browan.com>
2017-10-05 10:13:17 +0800

    add support OLD IDU downlink

--- Lab09_pub_public_mqtt_ClassC_LCD.js
+++ Lab09_pub_public_mqtt_ClassC_LCD.js
@@ -13,8 +13,8 @@
         "qos": "",
         "retain": "",
         "broker": "71f422e4.0f062c",
-        "x": 761,
-        "y": 224.444486618042,
+        "x": 880.0000076293945,
+        "y": 219.44448852539062,
         "wires": []
     },
     {
@@ -27,7 +27,7 @@
         "payloadType": "str",
         "repeat": "",
         "crontab": "",
-        "once": false,
+        "once": true,
         "x": 158.99998474121094,
         "y": 224.8888931274414,
         "wires": [
@@ -41,7 +41,7 @@
         "type": "function",
         "z": "51f949d9.b75c88",
         "name": "04000476 DownLink",
-        "func": "function addZero(i) {\n    if (i < 10) {\n        i = \"0\" + i;\n    }\n    return i;\n}\nvar d = new Date();\nvar h = addZero(d.getHours());\nvar m = addZero(d.getMinutes());\nvar s = addZero(d.getSeconds());\n//{\"correlationId\":\"B180C43D2FAB61\",\"dldata\":{\"macAddr\":\"04000476\",\"data\":\"13c51c18\",\"extra\":{\"port\":2, \"txpara\":\"6\"}}}\nvar payload = msg.payload;\nmsg.payload.dldata.data = h.toString()+\"c\"+m.toString() +\"c\"+ s.toString();\nmsg.payload.correlationId = Math.floor(Math.random()*4294967295).toString(16);\nmsg.payload.dldata.macAddr = \"004000476\";\nreturn msg;",
+        "func": "function addZero(i) {\n    if (i < 10) {\n        i = \"0\" + i;\n    }\n    return i;\n}\nvar d = new Date();\nvar h = addZero(d.getHours());\nvar m = addZero(d.getMinutes());\nvar s = addZero(d.getSeconds());\n//{\"correlationId\":\"B180C43D2FAB61\",\"dldata\":{\"macAddr\":\"04000476\",\"data\":\"13c51c18\",\"extra\":{\"port\":2, \"txpara\":\"6\"}}}\nvar payload = msg.payload;\nmsg.payload.dldata.data = h.toString()+\"c\"+m.toString() +\"c\"+ s.toString();\nmsg.payload.correlationId = Math.floor(Math.random()*4294967295).toString(16);\nmsg.payload.dldata.macAddr = \"04000476\";\nmsg.payload.dldata.extra.txpara = 34; //Old IDU Class C\n//msg.payload.dldata.extra.txpara = \"22\"; //New IDU & ODU Class C, Class C don't have comfirmed (\"26\")\nreturn msg;",
         "outputs": 1,
         "noerr": 0,
         "x": 548.0000190734863,
@@ -58,6 +58,7 @@
         "type": "json",
         "z": "51f949d9.b75c88",
         "name": "",
+        "pretty": false,
         "x": 356.9999771118164,
         "y": 224,
         "wires": [
@@ -71,11 +72,11 @@
         "type": "debug",
         "z": "51f949d9.b75c88",
         "name": "",
-        "active": true,
+        "active": false,
         "console": "false",
         "complete": "payload",
-        "x": 775.1666870117188,
-        "y": 352.1111145019531,
+        "x": 849.1666793823242,
+        "y": 287.11111545562744,
         "wires": []
     },
     {
@@ -86,11 +87,11 @@
         "topic": "client/200000020/200000020-GIOT-MAKER/dl-resp",
         "qos": "2",
         "broker": "71f422e4.0f062c",
-        "x": 430.1666564941406,
-        "y": 353.555588722229,
+        "x": 568.1666717529297,
+        "y": 347.55561447143555,
         "wires": [
             [
-                "a3ac6642.b2c198"
+                "c3789bbf.3f73e8"
             ]
         ]
     },
@@ -102,11 +103,11 @@
         "topic": "client/200000020/200000020-GIOT-MAKER/dl-result",
         "qos": "2",
         "broker": "71f422e4.0f062c",
-        "x": 420.1667022705078,
-        "y": 407.5555896759033,
+        "x": 575.1667327880859,
+        "y": 413.55561447143555,
         "wires": [
             [
-                "a3ac6642.b2c198"
+                "f374ee7f.8638d"
             ]
         ]
     },
@@ -114,11 +115,11 @@
         "id": "c71c05c6.0cd078",
         "type": "blynk-websockets-in-write",
         "z": "51f949d9.b75c88",
-        "name": "V1 Read",
+        "name": "V1 Terminal Read",
         "pin": "1",
         "client": "3c7f14a9.2b32dc",
-        "x": 206.50000381469727,
-        "y": 129.00000190734863,
+        "x": 129.50000381469727,
+        "y": 96.00000190734863,
         "wires": [
             [
                 "6526aad3.d6e294",
@@ -131,11 +132,11 @@
         "type": "debug",
         "z": "51f949d9.b75c88",
         "name": "",
-        "active": false,
+        "active": true,
         "console": "false",
         "complete": "true",
-        "x": 445.16669845581055,
-        "y": 103.11111259460449,
+        "x": 410,
+        "y": 100,
         "wires": []
     },
     {
@@ -143,11 +144,11 @@
         "type": "function",
         "z": "51f949d9.b75c88",
         "name": "04000476 DownLink",
-        "func": "//{\"correlationId\":\"B180C43D2FAB61\",\"dldata\":{\"macAddr\":\"04000476\",\"data\":\"13c51c18\",\"extra\":{\"port\":2, \"txpara\":\"6\"}}}\nvar blynk_data = msg.payload;\n//console.log(blynk_data);\nvar DL_payload = {\"correlationId\":\"B180C43D2FAB61\",\"dldata\":{\"macAddr\":\"04000476\",\"data\":\"13c51c18\",\"extra\":{\"port\":2, \"txpara\":\"26\"}}}\nmsg.payload = DL_payload;\nmsg.payload.dldata.data =  blynk_data;\nmsg.payload.correlationId = Math.floor(Math.random()*4294967295).toString(16);\nmsg.payload.dldata.macAddr = \"04000476\";\n//return blynk_data;\nreturn msg;",
+        "func": "//{\"correlationId\":\"B180C43D2FAB61\",\"dldata\":{\"macAddr\":\"04000476\",\"data\":\"13c51c18\",\"extra\":{\"port\":2, \"txpara\":\"6\"}}}\nvar blynk_data = msg.payload;\n//console.log(blynk_data);\nvar DL_payload = {\"correlationId\":\"B180C43D2FAB61\",\"dldata\":{\"macAddr\":\"04000476\",\"data\":\"13c51c18\",\"extra\":{\"port\":2, \"txpara\":\"26\"}}}\nmsg.payload = DL_payload;\nmsg.payload.dldata.data =  blynk_data;\nmsg.payload.correlationId = Math.floor(Math.random()*4294967295).toString(16);\nmsg.payload.dldata.macAddr = \"04000476\";\nmsg.payload.dldata.extra.txpara = 34; //Old IDU Class C\n//msg.payload.dldata.extra.txpara = \"22\"; //New IDU & ODU Class C, Class C don't have comfirmed (\"26\")\n//return blynk_data;\nreturn msg;",
         "outputs": "1",
         "noerr": 0,
-        "x": 501.00000762939453,
-        "y": 154.00000286102295,
+        "x": 545.0000076293945,
+        "y": 177.00000190734863,
         "wires": [
             [
                 "a9a9a215.4eba3",
@@ -163,8 +164,52 @@
         "active": true,
         "console": "false",
         "complete": "payload",
-        "x": 748.166675567627,
-        "y": 143.1111125946045,
+        "x": 872.1666793823242,
+        "y": 142.1111135482788,
+        "wires": []
+    },
+    {
+        "id": "c3789bbf.3f73e8",
+        "type": "debug",
+        "z": "51f949d9.b75c88",
+        "name": "",
+        "active": false,
+        "console": "false",
+        "complete": "false",
+        "x": 850.1666793823242,
+        "y": 347.88889503479004,
+        "wires": []
+    },
+    {
+        "id": "f374ee7f.8638d",
+        "type": "debug",
+        "z": "51f949d9.b75c88",
+        "name": "",
+        "active": false,
+        "console": "false",
+        "complete": "payload",
+        "x": 854.1666793823242,
+        "y": 413.88889503479004,
+        "wires": []
+    },
+    {
+        "id": "d6e8c776.5817c8",
+        "type": "comment",
+        "z": "51f949d9.b75c88",
+        "name": "更改 Project Key 為自己 blynk app 的 AUTH Token",
+        "info": "",
+        "x": 229.16665649414062,
+        "y": 54.777785301208496,
+        "wires": []
+    },
+    {
+        "id": "c1ca85c6.3044b8",
+        "type": "comment",
+        "z": "51f949d9.b75c88",
+        "name": "改MacAddr 為自己的",
+        "info": "",
+        "x": 532.1666870117188,
+        "y": 139.7777862548828,
         "wires": []
     },
     {
@@ -191,6 +236,6 @@
         "z": "",
         "name": "blynk-Android",
         "path": "ws://cloud.blynk.cc/websocket",
-        "key": "25a69250c5f045839ea912ca1c65ce2e"
+        "key": "ec2d29e38670478fbafec1966b709ef2"
     }
 ]
