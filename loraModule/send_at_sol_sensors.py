0ed2c8d88fbbe539af5828bc7d8b3ad9cef64bf0
Marty@LoRa MQTT Server <marty.chao@gmail.com>
2022-05-05 15:41:56 +0000

    fix some at command

--- loraModule/send_at_sol_sensors.py
+++ loraModule/send_at_sol_sensors.py
@@ -17,8 +17,10 @@ class LoraSerial(serial.Serial):
         self.clearBuffer()
         str = str + "\n"
         self.write(bytes(str.encode()))
-
-        time.sleep(0.1)  # [Important!] wait for 100 millisecond delay
+        if str == "at+cslrm":
+            time.sleep(5)
+        else:
+            time.sleep(0.1)  # [Important!] wait for 100 millisecond delay
         return self.getLine()
 
     # Read a line from serial & decode
@@ -50,33 +52,66 @@ if not ur_device:
     ur_device = int(input("which device?"))
 lora_module = LoraSerial(com_port_list[ur_device])
 # FW Version
-print(lora_module.sendLine("at+slmr?"))
+# print(lora_module.sendLine("at+slmr?"))
+print(lora_module.sendLine("at+sgmr?"))
 print(lora_module.sendLine("at+sgmm?"))
 # SF status
 # US915 0:SF10 1:SF9 2:SF8 3:SF7
 # AS923 0:SF12 1:SF11 2:SF10 3:SF9 4:SF8 5:SF7
-print(lora_module.sendLine("at+cadr=?"))
+# print(lora_module.sendLine("at+cadr=?"))
 print(lora_module.sendLine("at+cadr=3,5,FFFF,0,1"))
+# Save ADR value
+print(lora_module.sendLine("at+cslrm"))
 print(lora_module.sendLine("at+cadr?"))
 # Band
-ismb_list = lora_module.sendLine("at+cismb=?")
-ismb_dict = {x.split(":")[0]: x.split(":")[1] for x in ismb_list[1:-2]}
+"""
+at+cismb=?
++CISMB=<1-7>
+1:EU868
+2:US915
+3:AS923
+4:FULL
+5:AU915
+6:KR920
+7:ISR915
+8:RU864
+9:IND865
+OK
+
+"""
+# ismb_list = lora_module.sendLine("at+cismb=?")
+# print(ismb_list)
+# ismb_dict = {x.split(":")[0]: x.split(":")[1] for x in ismb_list[1:-2]}
+# print(ismb_dict)
+lora_module.clearBuffer()
+print(lora_module.sendLine("at+cismb?"))
 ismb_set = lora_module.sendLine("at+cismb?")[0].split(":")[1]
-print(f"ISMB:{ismb_dict[ismb_set]}")
+if ismb_set != "US915":
+    print(lora_module.sendLine("at+cismb=2"))
+print(f"ISMB:{ismb_set}")
+# print(f"ISMB:{ismb_dict[ismb_set]}")
 # ABP or OTAA
 print(lora_module.sendLine("at+cmode=?"))
+print(lora_module.sendLine("at+cmode=1"))
 print(lora_module.sendLine("at+cmode?"))
 # UpLink Ack
-print(lora_module.sendLine("at+csync=?"))
-print(lora_module.sendLine("at+csync?"))
+# print(lora_module.sendLine("at+csync=?"))
+# print(lora_module.sendLine("at+csync?"))
 # DEVEUI of OTAA Mode
-print(lora_module.sendLine("at+cdeveui?"))
+# print(lora_module.sendLine("at+cdeveui?"))
 # APPEUI of OTAA Mode
-print(lora_module.sendLine("at+cappkey?"))
+# print(lora_module.sendLine("at+cappkey?"))
 #  DEVADDR of ABP Mode
-print(lora_module.sendLine("at+cdevaddr?"))
+cdevaddr = lora_module.sendLine("at+cdevaddr?")[0].split(" ")[0].split(":")[1]
+print(cdevaddr)
+addr = input(f"New DEVADDR:[{cdevaddr}]") or cdevaddr
+if addr != cdevaddr:
+    print(lora_module.sendLine(f"at+cdevaddr={addr}"))
+print(lora_module.sendLine("at+cnwkskey=12345678901234567890123456789012"))
 print(lora_module.sendLine("at+cnwkskey?"))
+print(lora_module.sendLine("at+cappskey=12345678901234567890123456789012"))
 print(lora_module.sendLine("at+cappskey?"))
 # UpLink Freq List
-print(lora_module.sendLine("at+cqch?"))
+# print(lora_module.sendLine("at+cqch?"))
+print(lora_module.sendLine("at+cadr?"))
 print(lora_module.sendLine('AT+dtx=88,"8863599990090000004041351404135140413514041351404135140413514041351404135140413514041351"'))
